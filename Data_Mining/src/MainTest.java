3ab6fcac8fabb177132c2594a88443f4aeacb253
Mirko <127095167+CMirkode@users.noreply.github.com>
2023-05-05 16:54:56 +0200

    Add files via upload

--- Data_Mining/src/MainTest.java
+++ Data_Mining/src/MainTest.java
@@ -1,10 +1,13 @@
+import data.*;
+import mining.*;
+
 
 public class MainTest {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws OutOfRangeSampleSize {
 		
 		
 		Data data =new Data();
---
+++ Data_Mining/src/data/DiscreteItem.java
@@ -0,0 +1,16 @@
+package data;
+
+class DiscreteItem extends Item{
+
+    DiscreteItem(DiscreteAttribute attribute, String value){
+        super(attribute,value);
+    }
+
+    double distance(Object a){
+        if(getValue().equals(((DiscreteItem)a).getValue())){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+}
